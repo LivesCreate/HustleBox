893dcf91fbe2a26956592814b122bce17146d5e5
LivesCreate <252925942+LivesCreate@users.noreply.github.com>
2026-05-10 12:14:57 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 // HustleBox Service Worker
 // CACHE_NAME must be bumped on every release (matches VERSION in index.html)
-const CACHE_NAME = 'hustlebox-v1.2.0';
+const CACHE_NAME = 'hustlebox-v1.2.1';
 
 const ASSETS = [
   './',
